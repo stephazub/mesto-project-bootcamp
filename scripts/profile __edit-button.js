41444de74d193d4f3f8e5b4b85d5bb40d80e4b14
Stepa Zubenko <stepazubenko@gmail.com>
2023-06-18 21:13:53 +0300

    промежуточный коммит

--- scripts/profile __edit-button.js
+++ scripts/profile __edit-button.js
@@ -4,6 +4,7 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupCloseButton  = document.querySelector('.popup__close-button');
+
 /*Заполнение полей формы редактирования профиля информацией со страницы*/
 const profileFormInputs = document.querySelectorAll('.form__input');
 const profileName = document.querySelector('.profile__name');
@@ -11,6 +12,36 @@ const profileSubtitle = document.querySelector('.profile__subtitle');
 const formSubmit = document.querySelector('.form__submit');
 const form = document.querySelector('.form');
 
+/*Шесть карточек «из коробки»*/
+const card = document.querySelector('.element');
+const cards = document.querySelector('.elements');
+
+const cities = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 /*Функции*/
 
@@ -34,6 +65,16 @@ function handleFormSubmit(evt) {
   closePopup(popupElement);
 }
 
+/*Шесть карточек «из коробки»*/
+cities.forEach((element) => {
+  const cityCard = card.cloneNode(true);
+
+  cityCard.querySelector('.element__mesto').textContent = element.name;
+  cityCard.querySelector('.element__img').src = element.link;
+
+  cards.append(cityCard);
+})
+
 /*События*/
 /*Открытие закрытие модального окна редактироания профиля*/
 profileEditButton.addEventListener('click', function() {
@@ -46,3 +87,7 @@ popupCloseButton.addEventListener('click', function() {
 
 /*Редактирование имени и информации о себе*/
 form.addEventListener('submit', handleFormSubmit); 
+
+
+
+
