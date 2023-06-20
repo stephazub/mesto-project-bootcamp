e75e605b7ec8a7add96a4daaaaaf520c3a3d0428
Stepa Zubenko <stepazubenko@gmail.com>
2023-06-20 11:32:44 +0300

    Шесть карточек из коробки и добавление карточки

--- scripts/profile __edit-button.js
+++ scripts/profile __edit-button.js
@@ -1,21 +1,24 @@
-/*Переменные*/
+/*### ПЕРЕМЕННЫЕ ###*/
 
 /*Открытие закрытие модального окна редактироания профиля*/
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupElement = document.querySelector('.popup');
-const popupCloseButton  = document.querySelector('.popup__close-button');
+const popupElement = document.querySelectorAll('.popup');
+const popupCloseButton  = document.querySelectorAll('.popup__close-button');
 
 /*Заполнение полей формы редактирования профиля информацией со страницы*/
-const profileFormInputs = document.querySelectorAll('.form__input');
 const profileName = document.querySelector('.profile__name');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const formSubmit = document.querySelector('.form__submit');
-const form = document.querySelector('.form');
+const profileForm = document.querySelector('.form_button_profile');
+const profileInputs = document.querySelectorAll('.form__input_button_profile');
+const createForm = document.querySelector('.form_button_create');
+const createInputs = document.querySelectorAll('.form__input_button_create');
+
+/*Добавление карточки*/
+const createButton = document.querySelector('.profile__add-button')
 
 /*Шесть карточек «из коробки»*/
-const card = document.querySelector('.element');
+const card = document.querySelector('.card');
 const cards = document.querySelector('.elements');
-
 const cities = [
   {
     name: 'Архыз',
@@ -43,51 +46,77 @@ const cities = [
   }
 ];
 
-/*Функции*/
 
-/*Открытие закрытие модального окна редактироания профиля*/
-/*Заполнение полей формы редактирования профиля информацией со страницы*/
-function openPopup (popapElement) {
-  popapElement.classList.add('popup_status_opened');
-  profileFormInputs[0].value = profileName.textContent;
-  profileFormInputs[1].value = profileSubtitle.textContent;
+/*### ФУНКЦИИ ###*/
+
+/*Открытие модального окна редактироания профиля*/
+function openProfilePopup (popupElement) {
+  popupElement[0].classList.add('popup_status_opened');
+  profileInputs[0].value = profileName.textContent;
+  profileInputs[1].value = profileSubtitle.textContent;
 }
 
-function closePopup (popapElement) {
-    popapElement.classList.remove('popup_status_opened');
+/*Закрытие модального окна*/
+function closePopup (popupElement) {
+    popupElement[0].classList.remove('popup_status_opened');
+    popupElement[1].classList.remove('popup_status_opened');
 }
 
 /*Редактирование имени и информации о себе*/
-function handleFormSubmit(evt) {
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = profileFormInputs[0].value;
-  profileSubtitle.textContent = profileFormInputs[1].value;
+  profileName.textContent = profileInputs[0].value;
+  profileSubtitle.textContent = profileInputs[1].value;
   closePopup(popupElement);
 }
 
 /*Шесть карточек «из коробки»*/
 cities.forEach((element) => {
-  const cityCard = card.cloneNode(true);
-
+  const cityCard = card.content.cloneNode(true);
   cityCard.querySelector('.element__mesto').textContent = element.name;
   cityCard.querySelector('.element__img').src = element.link;
-
   cards.append(cityCard);
 })
 
-/*События*/
-/*Открытие закрытие модального окна редактироания профиля*/
+/*Открытие модального окна добавления карточки*/
+function openCreatePopup (popupElement) {
+  popupElement[1].classList.add('popup_status_opened');
+}
+
+/*Добавление карточки*/
+function handleCreateFormSubmit(evt) {
+  evt.preventDefault();
+  const cityCard = card.content.cloneNode(true);
+  cityCard.querySelector('.element__mesto').textContent = createInputs[0].value;
+  cityCard.querySelector('.element__img').setAttribute('src', createInputs[1].value);
+  cards.prepend(cityCard);
+  closePopup(popupElement);
+}
+
+/*### СЛУШАТЕЛИ ###*/
+
+/*Открытие модального окна редактироания профиля*/
 profileEditButton.addEventListener('click', function() {
-  openPopup(popupElement);
+  openProfilePopup(popupElement);
 });
 
-popupCloseButton.addEventListener('click', function() {
+/*Закрытие модального окна редактироания профиля*/
+popupCloseButton[0].addEventListener('click', function() {
   closePopup(popupElement);
 });
 
 /*Редактирование имени и информации о себе*/
-form.addEventListener('submit', handleFormSubmit); 
+profileForm.addEventListener('submit', handleProfileFormSubmit);
 
+/*Открытие модального окна добавления карточки*/
+createButton.addEventListener('click', function(){
+  openCreatePopup(popupElement)
+});
 
+/*Закрытие модального окна добавления карточки*/
+popupCloseButton[1].addEventListener('click', function() {
+  closePopup(popupElement);
+});
 
-
+/*Добавление карточки*/
+createForm.addEventListener('submit', handleCreateFormSubmit);
