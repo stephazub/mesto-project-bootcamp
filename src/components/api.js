1b7a37fec8ca272bd4aec6b5d8c47b7bcb96035f
stephazub <stepazubenko@outlook.com>
2023-07-10 18:45:48 +0300

    Добавление удаление лайка

--- src/components/api.js
+++ src/components/api.js
@@ -1,13 +1,56 @@
 const config = {
-    baseUrl: 'https://nomoreparties.co/v1/wbf-cohort-10',
+  baseUrl: 'https://nomoreparties.co/v1/wbf-cohort-10',
+  headers: {
+    authorization: 'f0c8e955-f497-444c-995d-f8a73ebe49d1',
+    'Content-Type': 'application/json'
+  },
+  profileNameSelector: '.profile__name',
+  profileAboutSelector: '.profile__subtitle',
+  profileAvatarSelector: '.profile__avatar',
+}
+
+//Постановка и снятие лайка
+function putLike(id) {
+  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+    method: 'PUT',
     headers: {
-      authorization: 'f0c8e955-f497-444c-995d-f8a73ebe49d1',
+      authorization: config.headers.authorization,
       'Content-Type': 'application/json'
-    },
-    profileNameSelector: '.profile__name',
-    profileAboutSelector: '.profile__subtitle',
-    profileAvatarSelector: '.profile__avatar',
-  }
+    }
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export { putLike }
+
+function deleteLike(id) {
+  return fetch(`${config.baseUrl}/cards/likes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      authorization: config.headers.authorization,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export { deleteLike }
 
 //Удаление карточки
 function deleteCard(id) {
@@ -17,10 +60,10 @@ function deleteCard(id) {
       authorization: config.headers.authorization,
       'Content-Type': 'application/json'
     }
-})
+  })
 }
 
-export {deleteCard};
+export { deleteCard };
 
 
 //Добавление новой карточки
@@ -36,15 +79,15 @@ function createNewCard(nameCard, linkCard) {
       link: linkCard
     })
   })
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Ошибка: ${res.status}`);
-   })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
 }
 
-export {createNewCard};
+export { createNewCard };
 
 
 //Редактирование профиля
@@ -62,62 +105,60 @@ function sendUserInfo() {
   })
 }
 
-export {sendUserInfo};
+export { sendUserInfo };
 
 //Загрузка информации о пользователе с сервера
 const profileName = document.querySelector(config.profileNameSelector);
 const profileAbout = document.querySelector(config.profileAboutSelector);
 const profileAvatar = document.querySelector(config.profileAvatarSelector);
 
-function getUserInfo () {
-  return fetch (`${config.baseUrl}/users/me`, {
+function getUserInfo() {
+  return fetch(`${config.baseUrl}/users/me`, {
     headers: {
       authorization: config.headers.authorization
     }
   })
-  .then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Ошибка: ${res.status}`);
-   })
-  .then((data) => {
-    console.log(data);
-    profileName.textContent = data.name;
-    profileAbout.textContent = data.about;
-    profileAvatar.setAttribute('src', data.avatar);
-    profileAvatar.setAttribute('alt', data.name);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-}
-
-export {getUserInfo};
-
-//Загрузка карточек с сервера
-function getInitialCards () {
-    return fetch (`${config.baseUrl}/cards`, {
-      headers: {
-        authorization: config.headers.authorization
-      }
-    })
     .then(res => {
       if (res.ok) {
         return res.json();
       }
       return Promise.reject(`Ошибка: ${res.status}`);
-     })
+    })
     .then((data) => {
-      console.log(data);
+      profileName.textContent = data.name;
+      profileAbout.textContent = data.about;
+      profileAvatar.setAttribute('src', data.avatar);
+      profileAvatar.setAttribute('alt', data.name);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export { getUserInfo };
+
+//Загрузка карточек с сервера
+function getInitialCards() {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: {
+      authorization: config.headers.authorization
+    }
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
+    .then((data) => {
+      console.log (data)
       return data;
     })
     .catch((err) => {
       console.log(err);
     });
-  }
-  
+}
+
 const cities = await getInitialCards();
 
-export {cities};
-  
+export { cities };
--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,6 @@
-import {openPopup, closePopup} from './modal';
-import {cities, createNewCard, deleteCard} from './api';
-
-console.log(cities);
+import { openPopup, closePopup } from './modal';
+import { cities, createNewCard, deleteCard, putLike, deleteLike } from './api';
+import { data } from 'autoprefixer';
 
 const createForm = document.forms.create;
 const popupAdd = document.querySelector('.popup_button_add');
@@ -16,65 +15,85 @@ const popupPhoto = document.querySelector('.popup__img');
 const myID = '26cb9d3e86c0b7951030edec';
 
 
-function addLikeListener(cityCard) {
-    cityCard.querySelector('.element__like').addEventListener('click', function (evt) {evt.target.classList.toggle('element__like_active')});
+function addLikeListener(cityCard, id, likeCounter) {
+  cityCard.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+    if (evt.target.classList.contains('element__like_active')) {
+      putLike(id);
+      likeCounter.textContent = Number(likeCounter.textContent) + 1;
+    } else {
+      deleteLike(id);
+      likeCounter.textContent = Number(likeCounter.textContent) - 1;
+    }
   }
+  );
+}
+
+function changeLike(cityCard, likesArray) {
+  if (likesArray.some(function (e) { return e._id == myID })) {
+    cityCard.querySelector('.element__like').classList.add('element__like_active');
+  } else {
+    cityCard.querySelector('.element__like').classList.remove('element__like_active');
+  }
+}
+
 
 function addDeleteListener(cityCard, id) {
-    cityCard.querySelector('.element__delete').addEventListener('click', function(evt) {
-      //evt.target.parentElement.remove();
-      deleteCard(id).then(() => {
-        evt.target.parentElement.remove();
-      });
+  cityCard.querySelector('.element__delete').addEventListener('click', function (evt) {
+    deleteCard(id).then(() => {
+      evt.target.parentElement.remove();
     });
-  }
-
-function createCard(title, ref, likes, owner, id) {
-    const cityCard = card.content.cloneNode(true);
-    cityCard.querySelector('.element__mesto').textContent = title;
-    cityCard.querySelector('.element__img').setAttribute('src', ref)
-    cityCard.querySelector('.element__img').setAttribute('alt', title);
-    cityCard.querySelector('.element__like-counter').textContent = likes;
-    const cardTrash = cityCard.querySelector('.element__delete');
-    hideTrash(cardTrash, owner);
-    addLikeListener(cityCard);
-    addDeleteListener(cityCard,id);
-    openImgPopap(cityCard, title, ref);
-    return cityCard;
-  }
-  
-function openImgPopap(cityCard, name, link) {
-    cityCard.querySelector('.element__img').addEventListener('click', function() {
-      openPopup(popupImg);
-      popupPhotoCaption.textContent = name;
-      popupPhoto.setAttribute('src', link);
-    })
-  }
-
-function showSixCards () {
-    cities.forEach((element) => {
-      const cityCard = createCard(element.name, element.link, element.likes.length, element.owner._id, element._id);
-      cards.append(cityCard);
-    });
-  }
-
-function handleCreateFormSubmit(evt, data) {
-  evt.preventDefault();
-    const cityCard = createCard(data.name, data.link, data.likes.length, data.owner._id, data._id);
-    cards.prepend(cityCard);
-    closePopup(popupAdd);
-  }
-
-function setListenerCreateForm (form) {
-  form.addEventListener('submit', function (evt) {
-     createNewCard(createInputName.value, createInputLink.value).then((data) => {
-      handleCreateFormSubmit(evt, data);
-    });
-     createForm.reset();
   });
 }
 
-export{showSixCards, setListenerCreateForm, createForm};
+function createCard(title, ref, likes, owner, id, likesArray) {
+  const cityCard = card.content.cloneNode(true);
+  cityCard.querySelector('.element__mesto').textContent = title;
+  cityCard.querySelector('.element__img').setAttribute('src', ref)
+  cityCard.querySelector('.element__img').setAttribute('alt', title);
+  const likeCounter = cityCard.querySelector('.element__like-counter');
+  likeCounter.textContent = likes;
+  const cardTrash = cityCard.querySelector('.element__delete');
+  hideTrash(cardTrash, owner);
+  addLikeListener(cityCard, id, likeCounter);
+  changeLike(cityCard, likesArray, likes)
+  addDeleteListener(cityCard, id);
+  openImgPopap(cityCard, title, ref);
+  return cityCard;
+}
+
+function openImgPopap(cityCard, name, link) {
+  cityCard.querySelector('.element__img').addEventListener('click', function () {
+    openPopup(popupImg);
+    popupPhotoCaption.textContent = name;
+    popupPhoto.setAttribute('src', link);
+  })
+}
+
+function showSixCards() {
+  cities.forEach((element) => {
+    const cityCard = createCard(element.name, element.link, element.likes.length, element.owner._id, element._id, element.likes);
+    cards.append(cityCard);
+  });
+}
+
+function handleCreateFormSubmit(evt, data) {
+  evt.preventDefault();
+  const cityCard = createCard(data.name, data.link, data.likes.length, data.owner._id, data._id, data.likes);
+  cards.prepend(cityCard);
+  closePopup(popupAdd);
+}
+
+function setListenerCreateForm(form) {
+  form.addEventListener('submit', function (evt) {
+    createNewCard(createInputName.value, createInputLink.value).then((data) => {
+      handleCreateFormSubmit(evt, data);
+    });
+    createForm.reset();
+  });
+}
+
+export { showSixCards, setListenerCreateForm, createForm };
 
 function hideTrash(cardTrash, owner) {
   if (owner !== myID) {
