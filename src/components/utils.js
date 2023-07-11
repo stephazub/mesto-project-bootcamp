46bba3d171a90bb703353386e2090f03cf475187
stephazub <stepazubenko@outlook.com>
2023-07-11 15:00:49 +0300

    Улучшенный UX всех форм

--- src/components/utils.js
+++ src/components/utils.js
@@ -13,9 +13,13 @@ const avatarInputLink = avatarForm.elements.link
 const avatarImage = document.querySelector('.profile__avatar');
 const popupAvatar = document.querySelector('.popup_button_avatar');
 
+const submitProfile = profileForm.elements.submit;
+const submitAvatar = avatarForm.elements.submit;
+
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
+  renderLoading(true, submitProfile);
   profileName.textContent = profileInputName.value;
   profileSubtitle.textContent = profileInputDescription.value;
   closePopup(popupProfile);
@@ -25,7 +29,7 @@ function handleProfileFormSubmit(evt) {
 function setListenerProfileForm(form) {
   form.addEventListener('submit', function (evt) {
     handleProfileFormSubmit(evt);
-    sendUserInfo();
+    sendUserInfo().finally(() => {renderLoading(false, submitProfile)});
   }
   )
 }
@@ -36,6 +40,7 @@ export { setListenerProfileForm, profileForm };
 //Обновление аватара пользователя
 function handleAvatarFormSubmit(evt) {
   evt.preventDefault();
+  renderLoading(true, submitAvatar);
   avatarImage.setAttribute('src', avatarInputLink.value);
   closePopup(popupAvatar);
 }
@@ -44,10 +49,19 @@ function setListenerAvatarForm(form) {
   form.addEventListener('submit', function (evt) {
     handleAvatarFormSubmit(evt);
     console.log(avatarImage.src);
-    editAvatar(avatarImage)
+    editAvatar(avatarImage).finally(() => {renderLoading(false, submitAvatar)});
     avatarForm.reset();
   }
   )
 }
 
 export { setListenerAvatarForm, avatarForm };
+
+
+function renderLoading(isLoading, submit) {
+  if (isLoading) {
+    submit.textContent = 'Сохранение...';
+  } else {
+    submit.textContent = 'Сохранить';
+  }
+} 
